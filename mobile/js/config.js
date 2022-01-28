e10ec54c878488982c49c5fad8546e9613ddb9f7
阿威 <68284698+mrnantang@users.noreply.github.com>
2022-01-28 17:16:19 +0800

    网站完成

--- mobile/js/config.js
+++ mobile/js/config.js
@@ -1,2 +1,27 @@
 let baseUrl = "http://2022.scxhlz.com/api/";
-export {baseUrl}
+
+function checkPlatform() {
+  var ua = window.navigator.userAgent.toLowerCase();
+  if (
+    !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+      navigator.userAgent
+    )
+  ) {
+    return "pc";
+  } else if (ua.indexOf("iphone") > 0 || ua.indexOf("android") > 0) {
+    return "mobile";
+  }
+  return "pc";
+}
+
+function ifJumpTo(url) {
+  let foundPlatform = checkPlatform();
+  let vars = url.split("/");
+  if (vars.length > 1) {
+    if (vars[1] != foundPlatform) {
+      window.location.href = url.replace(vars[1], foundPlatform);
+    }
+  }
+}
+
+export { baseUrl, ifJumpTo };
