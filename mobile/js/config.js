fd96148fe55395a02e0a539d579801bf7c60e644
阿威 <68284698+mrnantang@users.noreply.github.com>
2022-03-17 21:06:26 +0800

    Add files via upload

--- mobile/js/config.js
+++ mobile/js/config.js
@@ -1,4 +1,6 @@
 let baseUrl = "http://2022.scxhlz.com/api/";
+let maxClickTimesInOneDay = 1;//一天中对应同一个资源最大点赞数
+let maxClickTimesMsg = "今日点赞已达上限";
 
 function checkPlatform() {
   var ua = window.navigator.userAgent.toLowerCase();
@@ -7,14 +9,23 @@ function checkPlatform() {
       navigator.userAgent
     )
   ) {
-    return "pc";
+    // return "pc";
   } else if (ua.indexOf("iphone") > 0 || ua.indexOf("android") > 0) {
     return "mobile";
   }
-  return "pc";
+  // return "pc";
 }
 
-function ifJumpTo(url) {
+function ifJumpTo(url) {/*
+<<<<<<< .mine
+  // let foundPlatform = checkPlatform();
+  // let vars = url.split("/");
+  // if (vars.length > 1) {
+  //   if (vars[1] != foundPlatform) {
+  //     window.location.href = url.replace(vars[1], foundPlatform);
+  //   }
+  // }
+||||||| .r193
   let foundPlatform = checkPlatform();
   let vars = url.split("/");
   if (vars.length > 1) {
@@ -22,6 +33,14 @@ function ifJumpTo(url) {
       window.location.href = url.replace(vars[1], foundPlatform);
     }
   }
+=======
+  let foundPlatform = checkPlatform();
+  let vars = url.split("/");
+  if (vars.length > 1) {
+    if (vars[1] != foundPlatform) {
+      window.location.href = url.replace(vars[1], foundPlatform);
+    }
+  }*/
 }
 
-export { baseUrl, ifJumpTo };
+export { baseUrl, ifJumpTo, maxClickTimesInOneDay, maxClickTimesMsg };
--- pc/js/config.js
+++ pc/js/config.js
@@ -1,4 +1,6 @@
 let baseUrl = "http://2022.scxhlz.com/api/";
+let maxClickTimesInOneDay = 1;//一天中对应同一个资源最大点赞数
+let maxClickTimesMsg = "今日点赞已达上限";
 
 function checkPlatform() {
   var ua = window.navigator.userAgent.toLowerCase();
@@ -7,7 +9,7 @@ function checkPlatform() {
       navigator.userAgent
     )
   ) {
-    return "pc";
+    // return "pc";
   } else if (ua.indexOf("iphone") > 0 || ua.indexOf("android") > 0) {
     return "mobile";
   }
@@ -15,13 +17,13 @@ function checkPlatform() {
 }
 
 function ifJumpTo(url) {
-  let foundPlatform = checkPlatform();
-  let vars = url.split("/");
-  if (vars.length > 1) {
-    if (vars[1] != foundPlatform) {
-      window.location.href = url.replace(vars[1], foundPlatform);
-    }
-  }
+  // let foundPlatform = checkPlatform();
+  // let vars = url.split("/");
+  // if (vars.length > 1) {
+  //   if (vars[1] != foundPlatform) {
+  //     window.location.href = url.replace(vars[1], foundPlatform);
+  //   }
+  // }
 }
 
-export { baseUrl, ifJumpTo };
+export { baseUrl, ifJumpTo, maxClickTimesInOneDay, maxClickTimesMsg };
